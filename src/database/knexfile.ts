1c72bd7fe5c726778d7d027d30f823d3aeecf162
Kyle <kyleguerrero85@gmail.com>
2022-09-03 23:25:59 -0500

    chore: udpated scaffolding for knex, migrations and seeds

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,27 +1,67 @@
 interface IKnexConfig {
-  [key: string]: object
+  [key: string]: object;
 }
 
 const knexConfig: IKnexConfig = {
+  // development: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: `${__dirname}/dev.sqlite`,
+  //   },
+  //   migrations: {
+  //     directory: "./dev/migrations/",
+  //     tableName: "dbmigrations",
+  //   },
+  //   seeds: {
+  //     directory: "dev/seeds",
+  //   },
+  //   pool: {
+  //     afterCreate: (connection, done) => {
+  //       connection.run("PRAGMA foreign_keys = ON", done);
+  //     },
+  //   },
+  // },
   development: {
-    client: 'sqlite3',
+    client: "pg",
     useNullAsDefault: true,
-    connection: {
-      filename: './src/database/dev/dev.db3',
-    },
+    connection: process.env.DB_URI,
     migrations: {
-      directory: 'dev/migrations/',
-      tableName: 'dbmigrations',
+      directory: `${__dirname}/migrations`,
+      tableName: "knex_migrations",
     },
     seeds: {
-      directory: 'src/database/dev/seeds',
-    },
-    pool: {
-      afterCreate: (connection, done) => {
-        connection.run('PRAGMA foreign_keys = ON', done)
-      },
+      directory: `${__dirname}/seeds`,
     },
   },
-}
+  testing: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DB_URI_TESTING,
+    migrations: {
+      directory: `${__dirname}/migrations`,
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: `${__dirname}/seeds`,
+    },
+  },
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DB_URI,
+    migrations: {
+      directory: `${__dirname}/migrations`,
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: `${__dirname}/seeds`,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
 
-export default knexConfig
+export default knexConfig;
