db06af4f4795bd90cee77dd81a8eb3eacf420a6e
Nicholas O'Kelley <okelleynp@gmail.com>
2022-10-11 00:44:51 -0400

    fix: rewrite test message

--- src/api/resources/auth/__tests__/index.test.ts
+++ src/api/resources/auth/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import server from "../../../server";
 import request from "supertest";
 
 describe("Testing the Pulse Endpoint /", () => {
-  it('Returns status code 200 with message "Hello World"', async () => {
+  it('should return status code 200 with message "Hello World"', async () => {
     const res = await request(server).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual("Hello World!");
