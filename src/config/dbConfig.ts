cfaa3e159967de1246f3c4cd20f3668da372cc58
Nicholas O'Kelley <okelleynp@gmail.com>
2022-10-16 00:04:29 -0400

    fix: remove log

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,8 +1,6 @@
 import knex from "knex";
 import knexConfig from "../database/knexfile";
 
-console.log(`${process.env.DB_ENV}`)
-
 const env = process.env.DB_ENV || "localdev";
 
 export const db = knex(knexConfig[env]);
