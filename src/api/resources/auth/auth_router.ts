9e75df32fa8333120d2b6984b9601f0e5b4fe1c7
Nicholas O'Kelley <okelleynp@gmail.com>
2022-11-01 00:50:16 -0400

    docs: comments explaining verify funcs

--- src/api/resources/auth/auth_router.ts
+++ src/api/resources/auth/auth_router.ts
@@ -9,10 +9,14 @@ import {
 } from "../../middleware/verify_json";
 
 export let router: Router = Router();
+
+// For each route on this router, verify these two fields
+// exists.
 router.use(verify_username, verify_password);
 
 // Route to sign in and returns a JWT
 router.route("/").post(loginUser);
 
-// Route to signup a new user
+// Route to signup a new user, after ensure roleId exists, and the username
+// hasn't been used already
 router.route("/register").post(verify_role_id, validate_username, registerUser);
