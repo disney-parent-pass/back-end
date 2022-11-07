a7b3a5a94d242616e230de25fd61fc1b60e0116e
Kyle G <kyleguerrero85@gmail.com>
2022-11-06 22:21:58 -0600

    wip: started setting up testing for getting all posts in the DB

--- src/config/dbAccessFiles/post-model.ts
+++ src/config/dbAccessFiles/post-model.ts
@@ -5,6 +5,15 @@ class DbAccess {
   public getAllPosts() {
     return db(postTable);
   }
+
+  public async insertNewPost(newPost: any) {
+    const [id]: number[] = await db(postTable).insert(newPost, "id");
+    return this.findPostById(id);
+  }
+
+  public async findPostById(id: number) {
+    return db(postTable).where({ id }).first();
+  }
 }
 
 export default new DbAccess();
