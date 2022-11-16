332e3cd5630987b6618a93ce921dfe1b37780f11
Kyle G <kyleguerrero85@gmail.com>
2022-11-15 21:07:20 -0600

    chore: updated tests to use StatusCodes enum

--- src/api/resources/posts/__tests__/index.test.ts
+++ src/api/resources/posts/__tests__/index.test.ts
@@ -1,11 +1,12 @@
 import server from "../../../server";
 import request from "supertest";
+import StatusCodes from "../../../utils/status_codes";
 
 describe("/api/posts", () => {
   describe("Test fetching all posts", () => {
     it("should return a status code of 200", async () => {
       const response = await request(server).get("/api/posts");
-      expect(response.statusCode).toBe(200);
+      expect(response.statusCode).toBe(StatusCodes.OK);
     });
 
     it("should return a JSON payload", async () => {
