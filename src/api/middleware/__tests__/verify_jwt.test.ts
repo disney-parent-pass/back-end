69b52073afa2b61e86bc7bdfc0066aab6817f470
Nicholas O'Kelley <okelleynp@gmail.com>
2022-12-05 00:09:50 -0500

    feat: Add new unit test
    This unit test checks for the content type to be json and with
    a body that has the key 'message'.

--- src/api/middleware/__tests__/verify_jwt.test.ts
+++ src/api/middleware/__tests__/verify_jwt.test.ts
@@ -3,9 +3,16 @@ import request from "supertest";
 
 import StatusCodes from "../../utils/status_codes";
 
-describe("Testing a sample protected route", () => {
+describe("Testing a the /api/posts protected route", () => {
   it(`should return status code ${StatusCodes.UNAUTHORIZED} with message`, async () => {
     const res = await request(server).get("/api/posts/");
     expect(res.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
   });
+
+  it(`should be json format with message key and status code ${StatusCodes.UNAUTHORIZED}`, async () => {
+    const res = await request(server).get("/api/posts");
+    expect(res.get('Content-Type')).toEqual('application/json; charset=utf-8');
+    expect(res.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+    expect(res.body.message).toEqual("Not authorized, no token present");
+  })
 });
